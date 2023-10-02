0c3d98655f515317d29632f0a9c0440e329c0ecb
José Clodoalves <jrjcsj@gmail.com>
2023-10-02 17:48:54 -0300

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -10,17 +10,19 @@ void Matrix::Minus(Matrix anotherMatrix)
 
 }
 
-void Matrix::Plus(Matrix *anotherMatrix)
+void Matrix::Plus(Matrix* anotherMatrix)
 {
-    for (size_t i = 0; i < numberOfRows; i++)
-    {
-        for (size_t j = 0; j < numberOfColumns; j++)
-        {
-            float value = *(*anotherMatrix->elements + j);
+	auto foo = elements;
 
-            //*(elements + j) = 1 + value;
-        }        
-    } 
+	for (size_t i = 0; i < numberOfRows; i++)
+	{
+		for (size_t j = 0; j < numberOfColumns; j++)
+		{
+			float value = *(elements[i] + j);
+			float value2 = *(anotherMatrix->elements[i] + j);
+			*(elements[i] + j) = value + value2;
+		}
+	}
 }
 
 void Matrix::Times(float scalarElement)
@@ -40,17 +42,17 @@ void Matrix::Times(Matrix anotherMatrix)
 
 int Matrix::isIdentityMatrix()
 {
-    return 0;
+	return 0;
 }
 
 Matrix Matrix::GetInverseMatrix()
 {
-    return Matrix();
+	return Matrix();
 }
 
 Matrix Matrix::GetTransposeMatrix()
 {
-    return Matrix();
+	return Matrix();
 }
 
 void Matrix::Print()
@@ -60,20 +62,22 @@ void Matrix::Print()
 
 void Matrix::GenerateMatrix()
 {
-    for (int i = 0; i < numberOfRows; i++)
-    {   
-        for (int j = 0; j < numberOfColumns; j++)
-        {
-            float *line = elements[i];
-            line = new float();
-            float value;
-            cout << "Insert element [" << i + 1 <<","<< j + 1  << "] :" << endl;
-            cin >> value;
-            *(line + j) = value;
+	for (int i = 0; i < numberOfRows; i++)
+	{
+		for (int j = 0; j < numberOfColumns; j++)
+		{
+			/*     float *line = elements[i];
+				 line = new float();*/
 
-            //*(elements + j) = &value;
-        }
-    }
+			elements[i] = new float();
+			float value;
+			cout << "Insert element [" << i + 1 << "," << j + 1 << "] :" << endl;
+			cin >> value;
+			*(elements[i] + j) = value;
+
+			//*(elements + j) = &value;
+		}
+	}
 }
 
 // Matrix::Matrix(){}
